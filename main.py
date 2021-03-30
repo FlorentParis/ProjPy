84611240010ccf5a92be4d597c0c24c1a9933267
Giselle MATIAS <giselle.magalong@gmail.com>
2021-03-30 14:46:48 +0200

    Init display window

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-""" test """
+import pygame
+pygame.init()
+
+
+pygame.display.set_caption("Epic Noob Battle")
+screen = pygame.display.set_mode((1280, 600))
+
+background = pygame.image.load('assets/background.gif')
+
+running = True
+
+while running:
+  screen.blit(background, (0,0))
+
+  pygame.display.flip()
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+      pygame.quit()
+      print("Fermeture du jeu")
