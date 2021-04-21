a1320eba6ffefbf484e01ae848c1c09918de34c8
Florent <florentparis28@gmail.com>
2021-04-21 12:15:20 +0200

    Affichage pv + pseudo Game

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 from personnage import personnage
 import pygame
 
+FOND = pygame.Color(87, 95, 65)
+CLAIR = pygame.Color(140, 156, 93)
 
 class Game:
     def __init__(self):
@@ -12,8 +14,9 @@ class Game:
     def setPlayer(self, players):
         self.players = players
 
+    #Fonction deja existante dans Menu.py, faire un import
     def loadText(self, size, text):
-        font = pygame.font.SysFont(None, 64)
+        font = pygame.font.SysFont(None, size)
         img = font.render(text, True, "WHITE")
         return img
 
@@ -25,8 +28,14 @@ class Game:
                 #Appel fonction attribution carte x5 pour chaque joueur
             #else:
                 #Attribuer qu'une carte a chaque joueur
-            screen.blit(self.loadText(64, f"Manche {self.manche}"), (screenSize[0] / 2 - 400, 25))
-            print(ps[self.player1].health)
+            screen.blit(self.loadText(64, f"Manche {self.manche}"), (screenSize[0] / 2, screenSize[1] / 2))
+            pygame.draw.rect(screen,FOND,(screenSize[0] / 12,screenSize[1] / 24,360,15))
+            pygame.draw.rect(screen,CLAIR,(screenSize[0] / 12,screenSize[1] / 24,240,15))
+            pygame.draw.rect(screen,FOND,(screenSize[0] / 2 + screenSize[0] / 7,screenSize[1] / 24,360,15))
+            pygame.draw.rect(screen,CLAIR,(screenSize[0] / 2 + screenSize[0] / 7,screenSize[1] / 24,120,15))
+            screen.blit(self.loadText(24, ps[self.player1].name), (screenSize[0] / 12, screenSize[1] / 12))
+            screen.blit(self.loadText(72, 'VS'), (screenSize[0] / 2 - 36, screenSize[1] / 24))
+            screen.blit(self.loadText(24, ps[self.player1].name), (screenSize[0] - screenSize[0] / 6, screenSize[1] / 12))
             screen.blit(ps[self.player1].image,((screenSize[0] * 3 / 12) - personnage.SIZE[0] / 3, (screenSize[1] / 2) - personnage.SIZE[1] / 6))
             screen.blit(ps[self.player2].image, ((screenSize[0]*9/12)-personnage.SIZE[0]/3, (screenSize[1]/2)-personnage.SIZE[1]/5))
             #TODO a mettre dans une autre fonction (a mettre apres avoir choisie le gagnant
